fbc2567acdb5855d9e09c0aa60f6d02fc4d228e0
Zachary Ivie <zachary.ivie@gmail.com>
2025-06-22 08:24:00 -0400

    Added boilerplate for auto-trader module

---
+++ auto-trader/inferencer.py
@@ -0,0 +1,12 @@
+"""
+This class is responsible for responding to a user's messages with an LLM. 
+It should take in a list of messages and return a response.
+"""
+
+class Inferencer:
+    
+    def __init__(self):
+        pass
+    
+    def generate(self, messages: list[dict]):
+        pass
---
+++ auto-trader/vectorizor.py
@@ -0,0 +1,12 @@
+"""
+This class is responsible for vectorizing text. It should take in a string and return vector embeddings,
+which will be stored in postgres as a vector store and used for similarity search.
+"""
+
+class Vectorizor:
+    
+    def __init__(self):
+        pass
+    
+    def vectorize(self, text: str):
+        pass
